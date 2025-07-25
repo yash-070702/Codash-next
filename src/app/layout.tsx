af2e9974dbeb6f325237b6ed6312f69107260c45
Yash Kumar Aggarwal <yashaggarwal2002.ya@gmail.com>
2025-07-26 01:04:35 +0530

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
 import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Codash",
+  description: "Track your coding journey",
+  icons: {
+    icon: "/favicon.ico", // or /favicon.png or /favicon.svg
+  },
+};
  
 // These styles apply to every route in the application
 import './globals.css'
